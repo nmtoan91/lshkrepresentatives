19369ac004ad51083f3d001e7e3fac29574ee861
nmtoan91 <nmtoan91@gmail.com>
2023-08-11 10:30:37 +0900

    1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lshkrepresentatives", 
-    version="1.0.3",
+    version="1.0.4",
     author="nmtoan91",
     author_email="toan_stt@yahoo.com",
     description="A python package for LSH-k-Representatives algorithm",
