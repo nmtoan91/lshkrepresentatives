6d030647967919b2caf0c89b6fefb0a0133cdb39
nmtoan91 <nmtoan91@gmail.com>
2023-12-13 21:22:41 +0900

    1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lshkrepresentatives", 
-    version="1.0.9",
+    version="1.1.0",
     author="nmtoan91",
     author_email="toan_stt@yahoo.com",
     description="LSH-k-Representatives: A categorial clustering algorithm using LSH",
